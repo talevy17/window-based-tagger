948dec1cf03bbe30165927515a3b3e397a2ebe60
Tal Levy <tall@wix.com>
2019-12-13 11:44:47 +0200

    refactor model

--- tagger1.py
+++ tagger1.py
@@ -3,9 +3,10 @@ import torch.nn as nn
 
 
 class Model(nn.Module):
-    def __init__(self, batch_size, output_size, hidden_size, vocab_size, embedding_length,
-                 freeze_embeddings=False):
+    def __init__(self, batch_size, output_size, hidden_size, vocab_size, embedding_length, window_size, F2I):
         super(Model, self).__init__()
+        self.F2I = F2I
+        self.window_size = window_size
         self.batch_size = batch_size
         self.output_size = output_size
         self.hidden_size = hidden_size
@@ -14,13 +15,17 @@ class Model(nn.Module):
         torch.manual_seed(3)
         self.embedded = nn.Embedding(vocab_size, embedding_length)
         nn.init.uniform_(self.embedded.weight, -1.0, 1.0)
-        # self.word_embeddings = nn.Embedding.from_pretrained(torch.FloatTensor(self.embedded.weight), freeze=freeze_embeddings)
-        self.hidden = nn.Linear(5 * embedding_length, hidden_size)
+        self.hidden = nn.Linear(window_size * embedding_length, hidden_size)
         self.out = nn.Linear(hidden_size, output_size)
 
+    def concat(self, x):
+        concat_vector = []
+        for word in x:
+            concat_vector.append(self.embedded(self.F2I[word]))
+        return tuple(concat_vector)
+
     def forward(self, x):
-        data = torch.cat((self.embedded(x[0]), self.embedded(x[1]),
-                          self.embedded(x[2]), self.embedded(x[3]), self.embedded(x[4])), 0)
+        data = torch.cat(self.concat(x), 0)
         data = self.hidden(data)
         data = nn.functional.tanh(data)
         data = self.out(data)

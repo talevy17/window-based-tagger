ab6af72ebd663fbc9f45f3543b1e89c66791577e
Tal Levy <tall@wix.com>
2019-12-21 12:40:31 +0200

    refactor

--- top_k.py
+++ top_k.py
@@ -5,6 +5,7 @@ from DataUtils import FromPreTrained, UNKNOWN
 STUDENT = {'name': 'Tal Levy, Lidor Alis',
            'ID': '---, ---'}
 
+
 def cosine_distance(a, b):
     return np.divide(np.dot(a, b), (np.linalg.norm(a) * np.linalg.norm(b)))
 
